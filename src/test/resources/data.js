98673fae8c3271451d9874976d77f7a87bd45bdb
imankiit <im.ankiitsharma@gmail.com>
2021-04-11 23:49:05 +0530

    data to be inserted

---
+++ src/test/resources/data.js
@@ -0,0 +1,155 @@
+{
+	"cityName": "Hyderabad",
+	"theater": [{
+			"name": "PVR",
+			"totalSeats": 100,
+			"movies":
+			[{
+					"movieName": "Avenger",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "Money Heist",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "Spider Man",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "The Courier",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}
+			]
+		}, {
+			"name": "INOX",
+			"totalSeats": 100,
+			"movies":
+			[{
+					"movieName": "DDLJ",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "Baahubali",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "Burfi",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}, {
+					"movieName": "Tamasha",
+					"seat":
+					[{
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}, {
+							"booked": "N"
+
+						}
+					]
+				}
+			]
+
+		}
+	]
+
+}
